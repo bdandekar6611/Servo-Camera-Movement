2463885fe3551171c180da950df5a554c8ca9673
Rgidda4058 <51127135+Rgidda4058@users.noreply.github.com>
2019-06-20 09:05:05 -0400

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -244,11 +244,11 @@ try:		   # To handle the exceptions
             bol=False
         elif direct=='v':	#getting the camera preview commands from the ASCI keyboard word 'v'
             camera.start_preview()	#start the preview of the camera
-            camera.start_recording('/home/pi/Desktop/group1.h264')
-            time.sleep(15)
-            camera.stop_recording()
+            camera.start_recording('/home/pi/Desktop/group1.h264') # starts the vedio recording and save the file .h264 named as group1 on desktop
+            time.sleep(15)		#time set for the vedio recording 15 seconds
+            camera.stop_recording()	# stop the vedio recording
             camera.stop_preview()	#stop the preview of the camera
-except KeyboardInterrupt:			#if any other ASCI character press it interrupts the command
+except KeyboardInterrupt:		#if any other ASCI character press it interrupts the command
     gpio.cleanup()
 
 
