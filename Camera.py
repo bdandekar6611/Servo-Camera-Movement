b074429d869ed23a6642d3cf19f249d1d3443a78
Rgidda4058 <51127135+Rgidda4058@users.noreply.github.com>
2019-07-03 16:40:00 -0400

    Update Camera.py

--- Camera.py
+++ Camera.py
@@ -239,6 +239,7 @@ try:		   		# To handle the exceptions
             lowerServoRight()
         elif direct=='c':	#getting the capture mode commands from the ASCI keyboard word 'c'
             camera.capture('/home/pi/Desktop/group1.jpg')
+	    camera.stop_preview()
         elif direct=='b':	#getting the reset commands from the ASCI keyboard word 'b' 
             gpio.cleanup()
             bol=False
